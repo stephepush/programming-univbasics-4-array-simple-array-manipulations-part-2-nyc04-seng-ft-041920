cef815c9c89d5f6191d2155df3e2464ed66b6cec
Stephen Peters <7speter@gmail.com>
2020-04-27 22:54:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,21 +8,21 @@ def using_insert(array, item)
 end
 
 
-def using_uniq
-
+def using_uniq(array)
+    array.uniq
 end
 
 
-def using_flatten
-
+def using_flatten(array)
+    array.flatten
 end
 
 
-def using_delete
-
+def using_delete(array, string)
+  array.delete(string)
 end
 
 
-def using_delete_at
-
+def using_delete_at(array, index)
+    array.delete(index)
 end
